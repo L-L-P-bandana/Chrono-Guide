5442f61d8e9377eab94233fb778af5e4a11b4f95
L-L-P-bandana <84778907+L-L-P-bandana@users.noreply.github.com>
2025-02-28 11:09:54 +0000

    Update vite.config.js
    Corrected base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Chrono-Guide-Vercel-Patch-P/",
+  base: "/Chrono-Guide/",
   build: {
     outDir: "docs",
   },
